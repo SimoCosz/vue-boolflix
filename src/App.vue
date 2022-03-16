feb7512ce86df2f665cf19cae957ddf951fd03d6
coszach@hotmail.it <coszach@hotmail.it>
2022-03-16 10:43:19 +0100

    add library for flags

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
     
-      <input type="text" v-model="search">
+      <input type="text" v-model="search" @keyup.enter='getMovie(), getSeries()'>
       <button @click="getMovie(), getSeries()">Search</button>
       
       <h2>Movies</h2>
@@ -10,10 +10,8 @@
 
         <div class="card-movie" v-for="movie in movies" :key="movie.id">
           <h3 class="title">{{movie.title}}</h3>
-          <h4 class="original-title">{{movie.original_title}}</h4>
-          <figure>
-            <img :src="require(`./assets/img/${movie.original_language}.png`)" alt="">
-          </figure>
+          <p class="original-title">{{movie.original_title}}</p>
+          <p>{{getFlag(movie.original_language)}}</p>
           <p class="vote">{{movie.vote_average}}</p>
         </div>
       </div>
@@ -24,9 +22,7 @@
         <div class="card-movie" v-for="serie in series" :key="serie.id">
           <h3 class="title">{{serie.name}}</h3>
           <h4 class="original-title">{{serie.original_name}}</h4>
-          <figure>
-            <img :src="require(`./assets/img/${serie.original_language}.png`)" alt="">
-          </figure>
+          <p>{{getFlag(serie.original_language)}}</p>
           <p class="vote">{{serie.vote_average}}</p>
         </div>
       </div>
@@ -36,7 +32,10 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import getUnicodeFlagIcon from 'country-flag-icons/unicode';
+import { hasFlag } from 'country-flag-icons'
+
 
 export default {
   name: 'App',
@@ -49,7 +48,14 @@ export default {
       search: '',
       movies: [],
       series: [],
-      baseURL: 'https://api.themoviedb.org/3/search/'
+      baseURL: 'https://api.themoviedb.org/3/search/',
+      flags: {
+        en: require('./assets/img/en.png'),
+        it: require('./assets/img/it.png'),
+        es: require('./assets/img/es.png'),
+        no: require('./assets/img/no.png'),
+        pt: require('./assets/img/pt.png')
+      }
     }
   },
 
@@ -61,6 +67,17 @@ export default {
   },
 
   methods: {
+    getFlag: function(unicode){
+      if (unicode == 'en'){
+        unicode = 'gb';
+      }
+      if (hasFlag(unicode.toUpperCase())) {
+        return getUnicodeFlagIcon(unicode);
+      } else {
+        return unicode
+      }
+    },
+
     getMovie: function(){
       axios.get(`${this.baseURL}movie`,{
       params: {
@@ -104,6 +121,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #141414;
+  min-height: 100vh;
+  padding: 20px 0;
+
+  .container{
+    color: white;
+  }
 }
 
 .info-movie{
@@ -113,11 +137,11 @@ export default {
   margin: 20px 0;
 
   .card-movie{
-    border: 1px solid black;
-    width: calc((100% / 6) - 10px);
+    border: 1px solid white;
+    width: calc((100% / 5) - 10px);
     padding: 10px;
 
-    figure{
+    .flag{
       width: 20px;
     }
   }
